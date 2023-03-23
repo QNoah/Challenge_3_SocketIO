84439a76905910f657afef3f7c3b84b4a881441e
Quinten <90308786+QNoah@users.noreply.github.com>
2023-03-23 09:19:02 +0100

    Create server.js
    Server has been created with necassary code in it to make it work.

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const http = require('http')
+const express = require('express');
+const socketio = require('socketio');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+//static folder aanzetten..
+app.use(express.static(path.join(__dirname, 'public')));
+
+//run when client connects
+io.on('connection', socket =>{
+    console.log('New WS connection...');
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
