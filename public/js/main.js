379c42c7383e9458061145fe9e000baffc38edc4
Quinten <90308786+QNoah@users.noreply.github.com>
2023-03-23 14:32:18 +0100

    Messages get viewed in the chat.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,32 @@
+const chatForm = document.getElementById("chat-form");
+
 const socket = io();
 
+//berichten van de server
+socket.on("message", (message) => {
+  console.log(message);
+  outputMessage(message);
+});
 
-socket.on('message', message => {
-    console.log(message);
-})
+//Bericht versturen
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //bericht krijgen
+  const msg = e.target.elements.msg.value;
+
+  //message emit server
+  socket.emit("chatMessage", msg);
+});
+
+//berichten naar de DOM
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `
+    <p class="meta">Brad <span>9:12pm</span></p>
+						<p class="text">
+							${message}
+						</p>`;
+                        document.querySelector(".chat-messages").appendChild(div);
+}
--- server.js
+++ server.js
@@ -1,21 +1,32 @@
-const path = require('path');
-const http = require('http')
-const express = require('express');
-const socketio = require('socketio');
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-
 //static folder aanzetten..
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //run when client connects
-io.on('connection', socket =>{
-    console.log('New WS connection...');
+io.on("connection", (socket) => {
+  socket.emit("message", "Welcome to ChatQ");
+
+  //wanneer een persoon verbindt
+  socket.broadcast.emit("message", "A user has joined");
+
+  //when client disconnect
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has disconnected");
+  });
+  // Listen for chatMessages
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", msg);
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
-
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
